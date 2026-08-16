4477a55d07c7e8cc886e64f7981b23e2f3bab3ba
Chirag-V-11 <chiragvdevang@gmail.com>
2026-08-16 21:36:09 +0530

    p3

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {
-    root: __dirname,
+    root: path.resolve(__dirname, ".."),
   },
   async rewrites() {
     return [
